4e27ee39e6bf8f99de2d346c225102640fabe817
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-05-08 23:34:10 +0700

    Add GitHub CI

---
+++ .github/cmake/mingw-x86.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+include(${CMAKE_CURRENT_LIST_DIR}/mingw.cmake)
---
+++ .github/cmake/mingw-x86_64.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+include(${CMAKE_CURRENT_LIST_DIR}/mingw.cmake)
